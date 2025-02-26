756ff6bbe62f9ea81e92728a3586209384136c51
TS <ts@MacBook-Pro-mac-2.local>
2025-02-26 23:50:53 +0200

    fix: updated seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-"use client";
+// "use client";
 
 import { Suspense } from "react";
 
 import Head from "next/head";
-import { useSearchParams } from "next/navigation";
 
 import HeroSection from "./components/home/hero-section/HeroSection";
 import AdvantagesSection from "./components/home/advantages-section/AdvantagesSection";
@@ -16,17 +15,44 @@ import DeliverySection from "./components/home/delivery-section/DeliverySection"
 import SupportSection from "./components/home/support-section/SupportSection";
 import { translations } from "./utils/translations";
 
-export default function Home() {
-  const searchParams = useSearchParams();
-  const locale = searchParams.get("lang") as "en" | "pl" | "ua";
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { lang?: string };
+}) {
+  const locale = (searchParams.lang as "en" | "pl" | "ua") || "ua";
   const t = translations[locale] || translations.ua;
 
   return (
     <Suspense>
       <Head>
-        <title>{t.title}</title>
-
-        <meta name="description" content={t.description} />
+        <title>{t.title || "HeatKit - Pellet Burner Solutions"}</title>
+        <meta
+          name="description"
+          content={
+            t.description ||
+            "Sale of pellet burner in disassembled form. Save up to 50%!"
+          }
+        />
+        <meta
+          name="keywords"
+          content="пелетний пальник, економія на опаленні, HeatKit, енергетична зброя, пелети для опалення, купити пальник, самостійне збирання, екологічне опалення, енергоефективність, опалення будинку, пелетний котел, тепло для дому, дешеве опалення, якісні матеріали, теплова зброя, pellet burner, heating savings, energy weapon, pellet heating, buy burner, DIY assembly, eco-friendly heating, energy efficiency, home heating, pellet boiler, home warmth, cheap heating, high-quality materials, thermal weapon, palnik na pelety, oszczędność na ogrzewaniu, broń energetyczna, ogrzewanie pelletem, kup palnik, samodzielny montaż, ekologiczne ogrzewanie, efektywność energetyczna, ogrzewanie domu, kocioł na pelety, ciepło dla domu, tanie ogrzewanie, wysokiej jakości materiały, broń termiczna"
+        />
+        <link
+          rel="alternate"
+          href="https://heat-kit.vercel.app/?lang=en"
+          hrefLang="en"
+        />
+        <link
+          rel="alternate"
+          href="https://heat-kit.vercel.app/?lang=pl"
+          hrefLang="pl"
+        />
+        <link
+          rel="alternate"
+          href="https://heat-kit.vercel.app/?lang=ua"
+          hrefLang="uk"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
